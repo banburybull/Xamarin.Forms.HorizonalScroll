4cecf69f89ba66547bf4295885f2cb5c2a101a0d
banburybull <49441960+banburybull@users.noreply.github.com>
2019-08-15 11:28:07 +0100

    inital load of folders

---
+++ HorizontalScroll/SideScroll/SideScroll/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using SideScroll.Helpers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using SideScroll.Data;
+using System.Runtime.CompilerServices;
+
+namespace SideScroll
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        private MainPageViewModel _mainPageVM;
+       
+        public MainPage()
+        {
+            BindingContext = _mainPageVM = new MainPageViewModel();
+
+            InitializeComponent();
+        }
+
+       
+    }
+}
